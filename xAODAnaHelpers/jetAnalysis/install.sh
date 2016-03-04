da9ab7d146994ca0e67abe1bacdfa24e783f6d42
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-03-04 23:14:52 +0100

    more progres

--- xAODAnaHelpers/jetAnalysis/install.sh
+++ xAODAnaHelpers/jetAnalysis/install.sh
@@ -2,6 +2,7 @@
 source /atlas-asg/get_rcsetup.sh 
 rcSetup Base,2.3.38
 git clone https://github.com/UCATLAS/xAODAnaHelpers
+cd xAODAnaHelpers && git checkout 00-03-28 && cd -
 git clone https://github.com/kratsg/OriginCorrectedJets
 rc find_packages
 rc build
